9356f586f5e1cbfa2d9eb2966404f1449a068e4f
CrisBotu <c.bou.g@hotmail.com>
2016-08-11 16:46:23 +0200

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,11 @@
+## You should create one R script called run_analysis.R that does the following.
+
+## 1.Merges the training and the test sets to create one data set.
+## 2.Extracts only the measurements on the mean and standard deviation for each measurement.
+## 3.Uses descriptive activity names to name the activities in the data set
+## 4.Appropriately labels the data set with descriptive variable names.
+## 5.From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+
 rm(list = ls())
 
 # 1. Download the dataset if doesn't exist in the WD
@@ -28,6 +36,9 @@ if (!file.exists("UCI HAR Dataset")) {
 # 2. Load data activity and data feature info
 activityLabels  <- read.table("./UCI HAR Dataset/activity_labels.txt")
 features        <- read.table("./UCI HAR Dataset/features.txt")
+
+activityLabels[,2]  <- as.character(activityLabels[,2])
+features[,2]        <- as.character(features[,2])
 
 # 3. Extract the names, and cleaning data for mean and standard deviation
 extractFeatures       <- grep(".*mean.*|.*std.*",features[,2])
